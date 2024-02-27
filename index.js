41e322f4cc34f5a7ff019cf46df56ece75269f51
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-27 20:55:32 +0000

    finished server

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+	res.send("Hello world!");
+});
+
+app.get("/api", function (req, res) {
+	res.send({
+		small: "objects",
+		are: "easier",
+		to: "manage",
+		so: "are",
+		problems: " ",
+	});
+});
+
+app.get("/people", function (req, res) {
+	res.json(obj);
+});
+
+const obj = {
+	key1: "this string",
+	name: "fredrickson",
+	ocupation: "post",
+};
+
+app.listen(port, function () {
+	console.log(`listening on port ${port}`);
+});
